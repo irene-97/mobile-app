797127bd9dc1a062bcc9f17655d9df97573bcc71
Irene Artemova <artemova.irene@gmail.com>
2017-10-09 20:42:12 +0400

    Сверстала блок со слайдером

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,12 @@ svg4everybody();
 
 // @include('detect.js')
 // @include('globals.js')
+
+$('.slider .container').slick({
+	dots: true,
+	arrows: false,
+	infinite: true,
+	speed: 300,
+	slidesToShow: 1,
+	adaptiveHeight: true,
+});
