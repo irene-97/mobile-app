ca6b20696af1f8677c79dc8fc51c0b724a8cabd4
Irene Artemova <artemova.irene@gmail.com>
2017-10-10 20:45:38 +0400

    Добавила build

---
+++ build/js/main.js
@@ -0,0 +1,56 @@
+'use strict';
+
+svg4everybody();
+
+(function() {
+	var iterate = function iterate(items, callback) {
+		items.forEach(function(item) {
+			var key = void 0;
+			var value = void 0;
+
+			if (typeof item === 'string') {
+				key = item;
+				value = item;
+			} else {
+				key = item[0];
+				value = item[1];
+			}
+
+			callback(key, value);
+		});
+	};
+
+	var check = function check(category, items) {
+		iterate(items, function(key, value) {
+			if (bowser[key]) {
+				$(document.documentElement).addClass('is-' + category + '-' + value);
+			}
+		});
+	};
+
+	check('engine', ['webkit', 'blink', 'gecko', ['msie', 'ie'],
+		['msedge', 'edge']
+	]);
+
+	check('device', ['mobile', 'tablet']);
+
+	check('browser', ['chrome', 'firefox', ['msie', 'ie'],
+		['msedge', 'edge'], 'safari', 'android', 'ios', 'opera', ['samsungBrowser', 'samsung'], 'phantom', 'blackberry', 'webos', 'silk', 'bada', 'tizen', 'seamonkey', 'sailfish', 'ucbrowser', 'qupzilla', 'vivaldi', 'sleipnir', 'kMeleon'
+	]);
+
+	check('os', ['mac', 'windows', 'windowsphone', 'linux', 'chromeos', 'android', 'ios', 'iphone', 'ipad', 'ipod', 'blackberry', 'firefoxos', 'webos', 'bada', 'tizen', 'sailfish']);
+})();
+
+var $window = $(window);
+var $document = $(document);
+var $html = $(document.documentElement);
+var $body = $(document.body);
+
+$('.slider .container').slick({
+	dots: true,
+	arrows: false,
+	infinite: true,
+	speed: 300,
+	slidesToShow: 1,
+	adaptiveHeight: true
+});
